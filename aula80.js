15adc25241a9ddb7a9b9bae61cc72295d2abcb2a
EdvaldoUFRN <edvaldo_junior2009@hotmail.com>
2025-01-13 14:15:30 -0300

    parei na aula 87

---
+++ aula80.js
@@ -0,0 +1,8 @@
+const mat =document.getElementById("mat")
+//classe estatica então não precisamos instanciar um objeto da classe math.
+mat.innerHTML=Math.PI
+//tem o trunc que é para juntar.
+//tem a função random que gera um numero aleatorio entre 0 e 1.
+//se eu quiser q seja entre 10 e 0 a gente faz multiplicando ele por um valor, se quiser que ele apareça a gente adiciona um 1.
+//Math.floor() retorna o maior inteiro acima ou igual ao numero gerado. .
+//o round ele arredondaria então ele poderia chegar até 10.
