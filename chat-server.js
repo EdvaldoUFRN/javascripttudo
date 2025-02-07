594bb52d8a05110fbf369508d824416bfc8ee96f
edvaldoUFRN <edvaldo_junior2009@hotmail.com>
2025-02-07 20:16:53 -0300

    parei na criação do chat-server.js

--- chat-server.js
+++ chat-server.js
@@ -24,8 +24,32 @@ var server = net.createServer(function (socket) {
         // registre-o na saída do servidor
         process.stdout.write(message);
     });
+    //when client leaves
+    socket.on('end', function(){
+        var message =  clientName + ' left this chat\n';
+//log it to the server output
+process.stdout.write(message);
+//Remove client from socket array
+removeSocket(socket);
+//notify all clients
+broadcast(clientName, message);
+    });
+    //when socket gets errors 
+    socket.on('error',function(error){
+        console.log('Socket got problems: ', error.message);
+    });
+
+
+
+
+});
+
+//broadcast to others, excluding the sender
+function broadcast(from, message){
+    //if there are no sockets, then don't broadcast any messages
     
+}
 
-
-
-})
+//essa função net.createServer(function(socket){...}) cria um servidor tcp
+//e chama a função de callback toda vez que um cliente se conecta.
+//o parâmetro socket é um objeto que representa essa conexão específica entre o servidor e um cliente.
