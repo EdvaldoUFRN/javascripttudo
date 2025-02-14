c0db64294a9e39bea19224b743189b30ff5810d8
EdvaldoUFRN <edvaldo_junior2009@hotmail.com>
2025-02-14 13:16:48 -0300

    acabei o capítulo 10

--- Nodejs/10/app.js
+++ Nodejs/10/app.js
@@ -1,11 +1,14 @@
 let express = require('express');
 let app = express();
-//configura uma rota na raiz
-app.get('/',function(req,res){
-    res.send("API dos Carros");
-});
+let bodyParser = require('body-parser');
+//configura para ler os dados do POST por form-urlencoded e application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//Rotas
+app.use('/api/carros', require('./routes/carros')); //se eu quiser eu posso deixar apenas o / para permanecer a barra apenas.
+
 //inicia o servidor
-let server = app.listen(3000,function(){
+let server = app.listen(3000, function () {
     let host = server.address().address;
     let port = server.address().port;
     console.log("Servidor iniciado em http://%s:%s", host, port);
---
+++ Nodejs/10/routes/carros.js
@@ -0,0 +1,53 @@
+let express = require('express');
+const router = express.Router();
+const CarroDB = require('../model/CarroDB');
+//GET em /carros
+router.get('/', function (req, res) {
+    CarroDB.getCarros(function (carros) {
+        res.json(carros);
+    })
+});
+//GET em /carros/id
+router.get('/:id(\\d+)', function (req, res) {
+    let id = req.params.id;
+    CarroDB.getCarroById(id, function (carro) {
+        if (carro != undefined) {
+
+            res.json(carro);
+        } else {
+            res.json({ error: 'Nenhum carro foi encontrado!' });
+        }
+    });
+});
+//DELETE em /carros/id
+router.delete('/:id(\\d+)', function (req, res) {
+    let id = req.params.id;
+    console.log("deletar carro " + id);
+    CarroDB.deleteById(id, function (affectedRows) {
+        res.json({ msg: 'Carro deletado com sucesso.' });
+    });
+});
+//GET em /carros/esportivos
+router.get('/:tipo', function (req, res) {
+    let tipo = req.params.tipo;
+    res.send("Lista dos carros: " + tipo);
+})
+//POST para salvar um carro
+router.post('/', function (req, res) {
+    //carro enviado no formato json
+    let carro = req.body;
+    CarroDB.save(carro, function (carro) {
+        res.json(carro);
+    });
+});
+//PUT para atualizar um carro
+router.put('/', function (req, res) {
+    //carro enviado no formato json
+    let carro = req.body;
+    CarroDB.update(carro, function (carro) {
+        res.json({ msg: 'Carro atualizado com sucesso.' })
+    });
+});
+
+//exportar as rotas para ser utilizado em outro arquivo
+module.exports = router;
